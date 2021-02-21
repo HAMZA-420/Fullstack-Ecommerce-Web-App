7b5bb23dbb1860f50164a289c27aafb71db0852b
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-02-21 12:17:26 +0500

    sign In Component + FormInput

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../../singIn/sign-in.component';
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUp = () => (
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUp;
