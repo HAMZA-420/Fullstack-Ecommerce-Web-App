16dda49420913cb79b07eded7deb2915c39a1830
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-02-28 09:56:12 +0500

    Add to cart Styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './collection-item.styles.scss';
-
+import CustomButton from '../custom-button/custom-button.component';
 const CollectionItem =({id, name, price, imageUrl }) => (
     <div className='collection-item'>
         <div className='image'
@@ -12,6 +12,7 @@ const CollectionItem =({id, name, price, imageUrl }) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton inverted>Add to cart</CustomButton>
         </div>
     
 );
