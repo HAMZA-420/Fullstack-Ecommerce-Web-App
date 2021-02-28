1dadc51c3f462232823dab2d94d4466cb9b9f86b
MUHAMMAD HAMZA KHAN <mhamzakhan.cui@gmail.com>
2021-02-28 15:44:18 +0500

    updating with reselect ,memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,11 +8,15 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const CartIcon = ({toggleCartHidden}) => (
+const mapStateToProps = ({cart: {cartItems}}) =>({
+    itemCount: cartItems.reduce((accumalatedQuantity, cartItem)=> accumalatedQuantity +  cartItem.quantity, 0)
+})
+
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
